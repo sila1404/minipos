8144354a3c49f088805a39d7f5531daea5ec8ff1
sila1404 <silamany1404@gmail.com>
2023-11-29 20:25:49 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Vue App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Minipos13App',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
